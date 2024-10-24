4c3207a252aebb0cbadcc7586c4e9ea2814d7f8b
CrushedKatana <charelsadewo123@gmail.com>
2024-10-24 10:31:48 +0700

    commit

--- home.php
+++ home.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Laundry Admin</title>
+    <title>Home - Laundry Berkah Jaya</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .banner {
