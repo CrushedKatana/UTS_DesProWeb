9434cd72b196644e310063529e3a2a416777860b
CrushedKatana <charelsadewo123@gmail.com>
2024-10-24 09:43:13 +0700

    commit 2

--- login.js
+++ login.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
     $('#loginForm').submit(function(event) {
-        let valid = true;
-        let username = $('#username').val();
-        let password = $('#password').val();
-        
-        if (username === '') {
-            $('#usernameError').text('Must be filled');
-            valid = false;
-        } else {
-            $('#usernameError').text('');
-        }
+      let valid = true;
+      let username = $('#username').val();
+      let password = $('#password').val();
 
-        if (password === '') {
-            $('#passwordError').text('Must be filled');
-            valid = false;
-        } else if (password.length > 6) {
-            $('#passwordError').text('Password is more than 6 characters');
-            valid = false;
-        } else if (!/[A-Z]/.test(password) || !/[a-z]/.test(password)) {
-            $('#passwordError').text('Password must be uppercase and lowercase.');
-            valid = false;
-        } else {
-            $('#passwordError').text('');
-        }
+      // Username validation
+      if (username === '') {
+        $('#usernameError').text('Must be filled');
+        valid = false;
+      } else {
+        $('#usernameError').text('');
+      }
 
-        if (!valid) {
-            event.preventDefault();
-        }
+      // Password validation
+      if (password === '') {
+        $('#passwordError').text('Must be filled');
+        valid = false;
+      } else if (password.length < 6) {
+        $('#passwordError').text('Password must be at least 6 characters');
+        valid = false;
+      } else if (!/[A-Z]/.test(password) || !/[a-z]/.test(password)) {
+        $('#passwordError').text('Password must be uppercase and lowercase.');
+        valid = false;
+      } else {
+        $('#passwordError').text('');
+      }
+
+      // Prevent form submission if invalid
+      if (!valid) {
+        event.preventDefault();
+      }
     });
-});
+  });
